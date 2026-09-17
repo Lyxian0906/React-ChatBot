572a75a2aac7f09b306eb1737dbb128a6cf2e147
Lyx <martadiamond2@gmail.com>
2026-09-17 20:47:42 +0200

    Moved chat input

--- chatbot/src/App.jsx
+++ chatbot/src/App.jsx
@@ -1,53 +1,8 @@
 import { useState, useEffect, useRef } from 'react'
-import {Chatbot} from 'supersimpledev' 
 import './App.css'
+import robotPic from './assets/connor.webp'
+import userPic from './assets/fish.jpg'
 
-
-function ChatInput({ chatMessages, setChatMessages }) {
-				const [inputText, setInputText] = useState(""); //the value starts with empty
-				function saveInoutText(event) {
-					setInputText(event.target.value);
-				}
-
-				function sendMessage() {
-					const newChatMessages = [
-						...chatMessages,
-						{
-							message: inputText,
-							sender: "user",
-							id: crypto.randomUUID(),
-						},
-					];
-					//Wesave the new chat messages in the varible
-					setChatMessages(newChatMessages);
-
-					const response = Chatbot.getResponse(inputText); //This will gives us a response when we text (responses are in the chatbot.js)
-					setChatMessages([
-						...newChatMessages,
-						{
-							message: response,
-							sender: "robot",
-							id: crypto.randomUUID(),
-						},
-					]);
-
-					setInputText(""); //This will remove the input text from the text box, but won't update the html
-				}
-				return (
-					<div className="input-container1">
-						<input
-							placeholder="Send a message"
-							size="30"
-							onChange={saveInoutText}
-							value={inputText} //If we set now inputText to be empty will make the box to be empty
-							className="chat-input"
-						/>
-						<button onClick={sendMessage} className="send-button">
-							Send
-						</button>
-					</div>
-				);
-			}
 
 			function ChatMessage({ message, sender }) {
 				//const {message, sender} = props
@@ -74,11 +29,11 @@ function ChatInput({ chatMessages, setChatMessages }) {
 						}
 					>
 						{sender === "robot" && (
-							<img src="Photos/connor.webp" className="image-robot" />
+							<img src={robotPic} className="image-robot" />
 						)}
 						<div className="message">{message}</div>
 						{sender === "user" && (
-							<img src="Photos/fish.jpg" className="image-user" />
+							<img src={userPic} className="image-user" />
 						)}
 					</div>
 				);
---
+++ chatbot/src/components/ChatInput.jsx
@@ -0,0 +1,48 @@
+import { useState} from 'react'
+import {Chatbot} from 'supersimpledev' 
+
+function ChatInput({ chatMessages, setChatMessages }) {
+                const [inputText, setInputText] = useState(""); //the value starts with empty
+                function saveInoutText(event) {
+                    setInputText(event.target.value);
+                }
+
+                function sendMessage() {
+                    const newChatMessages = [
+                        ...chatMessages,
+                        {
+                            message: inputText,
+                            sender: "user",
+                            id: crypto.randomUUID(),
+                        },
+                    ];
+                    //Wesave the new chat messages in the varible
+                    setChatMessages(newChatMessages);
+
+                    const response = Chatbot.getResponse(inputText); //This will gives us a response when we text (responses are in the chatbot.js)
+                    setChatMessages([
+                        ...newChatMessages,
+                        {
+                            message: response,
+                            sender: "robot",
+                            id: crypto.randomUUID(),
+                        },
+                    ]);
+
+                    setInputText(""); //This will remove the input text from the text box, but won't update the html
+                }
+                return (
+                    <div className="input-container1">
+                        <input
+                            placeholder="Send a message"
+                            size="30"
+                            onChange={saveInoutText}
+                            value={inputText} //If we set now inputText to be empty will make the box to be empty
+                            className="chat-input"
+                        />
+                        <button onClick={sendMessage} className="send-button">
+                            Send
+                        </button>
+                    </div>
+                );
+            }

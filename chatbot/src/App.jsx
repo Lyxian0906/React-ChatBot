8cd41f6975aec58fa0e4efa34b1f149901980b7d
Lyx <martadiamond2@gmail.com>
2026-09-17 20:57:57 +0200

    Separated all modules from app

--- chatbot/src/App.jsx
+++ chatbot/src/App.jsx
@@ -1,67 +1,8 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState} from 'react'
 import './App.css'
-import robotPic from './assets/connor.webp'
-import userPic from './assets/fish.jpg'
+import { ChatInput } from './components/ChatInput'
+import  ChatMessages  from './components/ChatMessages'
 
-
-			function ChatMessage({ message, sender }) {
-				//const {message, sender} = props
-
-				//If sender is robot, we put the image of a , we change the side of the image by putting first the
-				//Shortcut below
-				/*
-							if (sender === "robot") {
-								return (
-									<div>
-										<img src="Photos/connor.webp" width="50" />
-										{message}
-									</div>
-								);
-							}
-			*/
-
-				//Else we just put the other image
-				//&& if the value on the left is true then the result will be the vaule on the right
-				return (
-					<div
-						className={
-							sender === "user" ? "chat-message-user" : "chat-message-robot"
-						}
-					>
-						{sender === "robot" && (
-							<img src={robotPic} className="image-robot" />
-						)}
-						<div className="message">{message}</div>
-						{sender === "user" && (
-							<img src={userPic} className="image-user" />
-						)}
-					</div>
-				);
-			}
-
-			function ChatMessages({ chatMessages }) {
-				const chatMessagesRef= useRef(null);
-				useEffect(() =>{
-					const containerElem= chatMessagesRef.current;
-					if(containerElem){
-						containerElem.scrollTop = containerElem.scrollHeight;
-					}
-				}, [chatMessages])
-				return (
-					<div className="chat-messages-container" ref={chatMessagesRef}>
-						{chatMessages.map((chatMessage) => {
-							return (
-								<ChatMessage
-									message={chatMessage.message}
-									sender={chatMessage.sender}
-									key={chatMessage.id}
-								/>
-							);
-						})}
-					</div>
-				);
-			}
-      
 function App() {
 				const [chatMessages, setChatMessages] = useState([
 					//We lifted the state up to app component
---
+++ chatbot/src/components/ChatMessage.jsx
@@ -0,0 +1,37 @@
+import robotPic from '../assets/connor.webp'
+import userPic from '../assets/fish.jpg'
+
+export function ChatMessage({ message, sender }) {
+                //const {message, sender} = props
+
+                //If sender is robot, we put the image of a , we change the side of the image by putting first the
+                //Shortcut below
+                /*
+                            if (sender === "robot") {
+                                return (
+                                    <div>
+                                        <img src="Photos/connor.webp" width="50" />
+                                        {message}
+                                    </div>
+                                );
+                            }
+            */
+
+                //Else we just put the other image
+                //&& if the value on the left is true then the result will be the vaule on the right
+                return (
+                    <div
+                        className={
+                            sender === "user" ? "chat-message-user" : "chat-message-robot"
+                        }
+                    >
+                        {sender === "robot" && (
+                            <img src={robotPic} className="image-robot" />
+                        )}
+                        <div className="message">{message}</div>
+                        {sender === "user" && (
+                            <img src={userPic} className="image-user" />
+                        )}
+                    </div>
+                );
+            }
